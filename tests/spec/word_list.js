94d0f14bd716bbdc8267f3324ce2d64310e2e200
Milomir Babic <redant333@gmail.com>
2022-03-20 07:20:54 +0100

    Add more tests

--- tests/spec/word_list.js
+++ tests/spec/word_list.js
@@ -12,12 +12,15 @@ describe("Word list", function() {
 // Tests
 function its(wordList) {
     let words = null;
+    let forms = null;
+    let infos = null;
 
     beforeEach(async () => {
         const data = await fetch(wordList);
         const json = await data.json();
         words = json["list"];
-        words = json["metadata"]["forms"];
+        forms = json["metadata"]["forms"];
+        infos = json["metadata"]["infos"];
     });
 
     it(wordList + " should contain at least two words", function() {
@@ -26,5 +29,23 @@ function its(wordList) {
 
     it(wordList + " should contain at least two forms", function() {
         expect(words.length).toBeGreaterThanOrEqual(2);
+    });
+
+    it(wordList + " should should have infos and forms for each word", function() {
+        for (const word of words) {
+            expect(word.length).toBe(2, word);
+        }
+    });
+
+    it(wordList + " should should have correct number of forms for each word", function() {
+        for (const [wordForms, _] of words) {
+            expect(wordForms.length).toBe(forms.length, wordForms);
+        }
+    });
+
+    it(wordList + " should should have correct number of infos for each word", function() {
+        for (const [_, wordInfos] of words) {
+            expect(wordInfos.length).toBe(infos.length, wordInfos);
+        }
     });
 }
