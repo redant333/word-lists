450021fc585e888b81a0e62fe60a43a18a77dd64
Milomir Babic <redant333@gmail.com>
2022-03-20 06:29:41 +0100

    Add basic word list tests

--- tests/spec/word_list.js
+++ tests/spec/word_list.js
@@ -1,7 +1,30 @@
 "use strict";
 
+// Suite
 describe("Word list", function() {
-    it("should do stuff", function() {
-        expect(1).toBe(1, "Something went wrong");
-    })
+    const wordLists = [
+        "/data/verb.json",
+    ]
+
+    for (const wordList of wordLists) {its(wordList) };
 });
+
+// Tests
+function its(wordList) {
+    let words = null;
+
+    beforeEach(async () => {
+        const data = await fetch(wordList);
+        const json = await data.json();
+        words = json["list"];
+        words = json["metadata"]["forms"];
+    });
+
+    it(wordList + " should contain at least two words", function() {
+        expect(words.length).toBeGreaterThanOrEqual(2);
+    });
+
+    it(wordList + " should contain at least two forms", function() {
+        expect(words.length).toBeGreaterThanOrEqual(2);
+    });
+}
