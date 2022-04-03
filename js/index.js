2acfa983c94674d914f1a9d12b25bfa351347826
Milomir Babic <redant333@gmail.com>
2022-04-03 18:39:02 +0200

    Load word lists dynamically

---
+++ js/index.js
@@ -0,0 +1,26 @@
+"use strict";
+
+function createWordListEntryNode(title, description, listName) {
+    const entryTemplate = document.getElementById("id_word_list_entry");
+    const entryNode = entryTemplate.content.cloneNode(true);
+
+    entryNode.querySelector(".data-title").innerText = title;
+    entryNode.querySelector(".data-description").innerHTML = description;
+    entryNode.querySelectorAll(".data-link").forEach(anchor => {
+        anchor.href += listName;
+    });
+
+    return entryNode;
+}
+
+function loadListEntries(containerId) {
+    fetch("/word-lists/data/index.json")
+    .then(data => {return data.json()})
+    .then(json => {
+        const container = document.getElementById(containerId);
+        for (const entry of json) {
+            const entryNode = createWordListEntryNode(entry["title"], entry["description"], entry["list_name"]);
+            container.appendChild(entryNode);
+        }
+    });
+}
